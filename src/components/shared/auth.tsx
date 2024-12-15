df82d62b6128413ead83908f809c20b8ddfb72ee
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-12-16 00:17:03 +0400

    authentication is done

---
+++ src/components/shared/auth.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import Link from 'next/link'
+
+const Auth = () => {
+  const { isAuthenticated } = useKindeBrowserClient()
+  return (
+    <>
+      {isAuthenticated ? (
+        <Link
+          href='/api/auth/logout'
+          className='px-3 py-1 rounded-[5px] bg-transparent hover:bg-zinc-100 
+      transition-all duration-200 ease-linear text-[15px] text-zinc-900'
+        >
+          Sign out
+        </Link>
+      ) : (
+        <Link
+          href='/api/auth/register'
+          className='px-3 py-1 rounded-[5px] bg-transparent hover:bg-zinc-100 transition-all duration-200 ease-linear text-[15px] text-zinc-900'
+        >
+          Sign in
+        </Link>
+      )}
+    </>
+  )
+}
+
+export default Auth
