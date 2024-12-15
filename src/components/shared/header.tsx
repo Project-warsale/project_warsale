9baf62c63c5b22891261ed34e7d2a13c4592d88f
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-12-15 23:08:34 +0400

    last changes

---
+++ src/components/shared/header.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header className='w-full flex items-center justify-between gap-5'>
+      <Link href='/'></Link>
+    </header>
+  )
+}
+
+export default Header
